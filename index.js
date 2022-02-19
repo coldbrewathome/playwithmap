a4fd85c17a637596cfc546040acfa422f0a03496
coldbrewathome <55669492+coldbrewathome@users.noreply.github.com>
2022-02-19 15:24:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
+/*
+ * declare map as a global variable
+ */
+var map;
+
+/*
+ * use google maps api built-in mechanism to attach dom events
+ */
+google.maps.event.addDomListener(window, "load", function () {
+
+  /*
+   * create map
+   */
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(47.791846, 22.876955),
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
+
+  /*
+   * create infowindow (which will be used by markers)
+   */
+  var infoWindow = new google.maps.InfoWindow();
+
+  /*
+   * marker creater function (acts as a closure for html parameter)
+   */
+  function createMarker(options, html) {
+    var marker = new google.maps.Marker(options);
+    if (html) {
+      google.maps.event.addListener(marker, "click", function () {
+        infoWindow.setContent(html);
+        infoWindow.open(options.map, this);
+      });
+    }
+    return marker;
+  }
+
+  /*
+   * add markers to map
+   */
+  var marker0 = createMarker({
+    position: new google.maps.LatLng(47.791846, 22.876955),
     map: map,
-  });
-}
+    icon: "http://1.bp.blogspot.com/_GZzKwf6g1o8/S6xwK6CSghI/AAAAAAAAA98/_iA3r4Ehclk/s1600/marker-green.png"
+  }, "<p>BOITOR ORSOLYA, CABINET MEDICAL INDIVIDUAL</p>");
+
+});
+
+
+
+// Initialize and add the map
+// function initMap() {
+//   // The location of Uluru
+//   const uluru = { lat: -25.344, lng: 131.036 };
+//   // The map, centered at Uluru
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: uluru,
+//   });
+//   // The marker, positioned at Uluru
+//   const marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map,
+//   });
+// }
