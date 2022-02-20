1ba95686c45c66f6f90e28da809cdf95d0e8cc22
coldbrewathome <55669492+coldbrewathome@users.noreply.github.com>
2022-02-19 16:32:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 2,
-    center: new google.maps.LatLng(2.8, -187.3),
+    center: new google.maps.LatLng(37.42292240075573, -122.08429209566403),
     mapTypeId: "terrain",
   });
 
@@ -13,15 +13,15 @@ function initMap() {
   // This example uses a local copy of the GeoJSON stored at
   // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
   script.src =
-    "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+    "data/bayarea.js";
   document.getElementsByTagName("head")[0].appendChild(script);
 }
 
 // Loop through the results array and place a marker for each
 // set of coordinates.
 const eqfeed_callback = function (results) {
-  for (let i = 0; i < results.features.length; i++) {
-    const coords = results.features[i].geometry.coordinates;
+  for (let i = 0; i < results.data.length; i++) {
+    const coords = results.data[i].position;
     const latLng = new google.maps.LatLng(coords[1], coords[0]);
 
     new google.maps.Marker({
