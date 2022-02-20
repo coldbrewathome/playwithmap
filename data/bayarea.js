947536ca191d340df6493fcd01d65cd5ed5b0bcf
coldbrewathome <55669492+coldbrewathome@users.noreply.github.com>
2022-02-19 16:32:42 -0800

    Create bayarea.js

---
+++ data/bayarea.js
@@ -0,0 +1,8 @@
+eqfeed_callback(
+  {
+    data: [
+      {id: 1, position: {37.42292240075573, -122.08429209566403}, plus: "CWC8+R9", name: "google", description: ""},
+      {id: 2, position: {37.485272871889215, -122.14832559235714}, name: "facebook", description: ""}
+    ]
+  }
+)
